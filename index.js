150778992c2d853b9f1624d0d39129fd085d4fac
Gianpaolo Macerola <gmacerola@gmail.com>
2020-07-23 10:29:56 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,33 @@
+
+//form event.preventDefault();
+function addListItem () {
+$("#js-shopping-list-form").submit(event => {
+    event.preventDefault();
+    const addItem = $(event.currentTarget).find("#shopping-list-entry").val();
+    $(".shopping-list").append(
+        '<li><span class="shopping-item">' + addItem + '</span><div class="shopping-item-controls"><button class="shopping-item-toggle"><span class="button-label">check</span></button><button class="shopping-item-delete"><span class="button-label">delete</span></button></div>'
+    )
+    $("#js-shopping-list-form")[0].reset();
+}
+)
+}
+
+function removeItem () {
+    $(".shopping-list").on("click" , ".shopping-item-delete" , function(event) {
+        $(this).parent().parent().remove();
+    }
+    )
+}
+
+function check () {
+    $(".shopping-list").on("click" , ".shopping-item-toggle" , function(event) {
+        console.log("Button Pressed");
+        $(this).parent().parent(".shopping-item").toggleClass(".shopping-item__checked");
+    }
+    )
+}
+
+
+addListItem();
+removeItem();
+check();
